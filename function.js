1d218336efe6040c078191908534881d790d74db
Leavism <ghuydang@gmail.com>
2019-06-12 19:07:21 +0700

    Rewrite for organization and optimisation

--- function.js
+++ function.js
@@ -1,249 +1,174 @@
-let startBtn = document.querySelectorAll(".startBtn, .startBtn.darkBtn");
+const log = document.querySelector(".log");
+let action = document.querySelector(".action");
+let score = document.querySelector(".score");
+let dark = false;
 
-startBtn.forEach(sButton => {
-  sButton.addEventListener("click", start);
-});
+initiate();
 
-let log = document.querySelector("log");
-let choices = document.querySelector("choices");
-let score = document.querySelector("score");
+function initiate() {
+  updateLog("you approach the computer.", 1000);
+  updateLog("it's offline.", 1500);
 
-let playerChoices = document.createElement("playerChoices");
-let scissorBtn = makeBtn("scissor");
-let paperBtn = makeBtn("paper");
-let rockBtn = makeBtn("rock");
-playerChoices.append(rockBtn);
-playerChoices.append(paperBtn);
-playerChoices.append(scissorBtn);
+  let start = createChoices("", "start");
+  action.appendChild(start);
 
-let options = ["rock", "paper", "scissor"];
-
-let scoreLabel = document.createElement("label");
-scoreLabel.textContent = "score";
-
-let pScore = makeScore("player");
-let cScore = makeScore("computer");
-
-let rCount = document.createElement("div");
-rCount.classList = "rCount";
-rCount.textContent = "round";
-let rValue = document.createElement("span");
-rValue.classList = "rValue";
-rValue.textContent = "0/5";
-rCount.append(rValue);
-
-let choiceLabel = document.createElement("label");
-choiceLabel.textContent = "choices";
-
-function makeScore(name) {
-  let score = document.createElement("div");
-  score.classList = `${name[0]}Score`;
-  score.textContent = name;
-
-  let value = document.createElement("span");
-  value.classList = `${name[0]}Value`;
-  value.textContent = "0";
-  score.append(value);
-  return score;
-}
-
-function makeBtn(name) {
-  let button = document.createElement("button");
-  button.classList.add(`${name}Btn`);
-  button.textContent = titleCase(name);
-  return button;
-}
-
-function start() {
-  let startP = document.createElement("p");
-  startP.textContent = "the computer turns on.";
-
-  let prompt1 = document.createElement("p");
-  prompt1.textContent = "it awaits your choice.";
-
-  startBtn.forEach(sButton => {
-    sButton.remove();
-  });
-
-  log.prepend(startP);
-
-  setTimeout(function() {
-    log.prepend(prompt1);
-    addChoices();
-    addScores();
-    addLabels();
+  start.addEventListener("click", () => {
+    action.removeChild(start);
     game();
-  }, 1000);
+  });
 }
 
 function game() {
-  let playerScore = parseInt(
-    document.querySelector('span[class="pValue"]').textContent
-  );
-  let computerScore = parseInt(
-    document.querySelector('span[class="cValue"]').textContent
-  );
-  let round = 0;
+  let playerScore = 0;
+  let comScore = 0;
+  let roundNumber = 0;
 
-  rockBtn.addEventListener("click", function() {
-    let computerChoice = computerPlay();
-    let playerChoice = "rock";
-    let promptRock = document.createElement("p");
-    promptRock.textContent = "you choose rock.";
-    log.prepend(promptRock);
+  updateLog("the computer turns on.", 500);
+  updateLog("it awaits your choice.", 1000);
 
-    let result = calculator(playerChoice, computerChoice);
+  let rps = createChoices("choices", "rock", "paper", "scissor");
+  action.appendChild(rps);
 
-    updateLog(`the computer chooses ${computerChoice}.`, 500);
+  createScores("you", "computer");
 
-    if (result) {
-      playerScore++;
-      updateLog("you score.", 1000);
-    } else if (result === null) {
-      updateLog("neither scored.", 1000);
-    } else {
-      computerScore++;
-      updateLog("the computer scores.", 1000);
-    }
-
-    updateScores(playerScore, computerScore);
-    round++;
-    checkEnd(round, playerScore, computerScore);
+  let rockBtn = document.querySelector(".rockBtn");
+  rockBtn.addEventListener("click", () => {
+    updateLog(`you chose rock.`, 250);
+    tempDisable(rockBtn, 1000);
+    let result = playRound("rock", roundNumber, playerScore, comScore);
+    roundNumber = result.roundNumber;
+    playerScore = result.playerScore;
+    comScore = result.comScore;
+    checkRound(roundNumber, comScore, playerScore, rps);
+    fade();
+    console.log(roundNumber);
   });
 
-  paperBtn.addEventListener("click", function() {
-    let computerChoice = computerPlay();
-    let playerChoice = "paper";
-    let promptPaper = document.createElement("p");
-    promptPaper.textContent = "you choose paper.";
-    log.prepend(promptPaper);
-
-    let result = calculator(playerChoice, computerChoice);
-
-    updateLog(`the computer chooses ${computerChoice}.`, 500);
-
-    if (result) {
-      playerScore++;
-      updateLog("you score.", 1000);
-    } else if (result === null) {
-      updateLog("neither scored.", 1000);
-    } else {
-      computerScore++;
-      updateLog("the computer scores.", 1000);
-    }
-
-    updateScores(playerScore, computerScore);
-    round++;
-    checkEnd(round, playerScore, computerScore);
+  let paperBtn = document.querySelector(".paperBtn");
+  paperBtn.addEventListener("click", () => {
+    let result = playRound("paper", roundNumber, playerScore, comScore);
+    updateLog(`you chose paper.`, 250);
+    tempDisable(paperBtn, 1000);
+    roundNumber = result.roundNumber;
+    playerScore = result.playerScore;
+    comScore = result.comScore;
+    checkRound(roundNumber, comScore, playerScore, rps);
+    fade();
   });
 
-  scissorBtn.addEventListener("click", function() {
-    let computerChoice = computerPlay();
-    let playerChoice = "scissor";
-    let promptscissor = document.createElement("p");
-    promptscissor.textContent = "you choose scissor.";
-    log.prepend(promptscissor);
-
-    let result = calculator(playerChoice, computerChoice);
-
-    updateLog(`the computer chooses ${computerChoice}.`, 500);
-
-    if (result) {
-      playerScore++;
-      updateLog("you score.", 1000);
-    } else if (result === null) {
-      updateLog("neither scored.", 1000);
-    } else {
-      computerScore++;
-      updateLog("the computer scores.", 1000);
-    }
-
-    updateScores(playerScore, computerScore);
-    round++;
-    checkEnd(round, playerScore, computerScore);
+  let scissorBtn = document.querySelector(".scissorBtn");
+  scissorBtn.addEventListener("click", () => {
+    let result = playRound("scissor", roundNumber, playerScore, comScore);
+    updateLog(`you chose scissor.`, 250);
+    tempDisable(scissorBtn, 1000);
+    roundNumber = result.roundNumber;
+    playerScore = result.playerScore;
+    comScore = result.comScore;
+    checkRound(roundNumber, comScore, playerScore, rps);
+    fade();
   });
 }
 
-function updateLog(text) {
-  if (arguments[1] === undefined) {
-    var wait = 750;
-  } else var wait = arguments[1];
-  let genPrompt = document.createElement("p");
-  genPrompt.textContent = text;
-  setTimeout(function() {
-    log.prepend(genPrompt);
-  }, wait);
-
-  let nListCount = log.childElementCount;
-  if (nListCount > 20) {
-    log.removeChild(log.lastElementChild);
-    log.removeChild(log.lastElementChild);
-  } else if (nListCount > 22) {
-    log.removeChild(log.lastElementChild);
-    log.removeChild(log.lastElementChild);
-    log.removeChild(log.lastElementChild);
-    log.removeChild(log.lastElementChild);
-  }
-}
-
-function updateScores(playerScore, computerScore) {
-  let cValue = cScore.firstElementChild;
-  let pValue = pScore.firstElementChild;
-  pValue.textContent = playerScore;
-  cValue.textContent = computerScore;
-}
-
-function addChoices() {
-  choices.append(playerChoices);
-}
-
-function removeChoices() {
-  while (choices.firstElementChild) {
-    choices.removeChild(choices.firstElementChild);
-  }
-}
-
-function checkEnd(round, pScore, cScore) {
-  rValue.textContent = `${round}/5`;
-  if (round >= 5) {
-    removeChoices();
-    if (pScore == cScore) {
-      updateLog("it was a tie.");
+function checkRound(round, cScore, pScore, rps) {
+  if (round == 5) {
+    if (cScore > pScore) {
+      updateLog("the computer wins.");
+      action.removeChild(rps);
     } else if (pScore > cScore) {
       updateLog("you win.");
+      action.removeChild(rps);
     } else {
-      updateLog("the computer wins.");
+      updateLog("neither win.");
+      action.removeChild(rps);
     }
-  } else return;
+  }
+  let roundValue = document.querySelector(".roundValue");
+  roundValue.textContent = `${round}/5`;
 }
 
-function addScores() {
-  score.append(pScore);
-  score.append(cScore);
-  score.append(rCount);
+function createScores() {
+  let scoreTitle = document.createElement("label");
+  scoreTitle.textContent = "score";
+  score.appendChild(scoreTitle);
+
+  for (let i = 0; i < arguments.length; i++) {
+    let point = document.createElement("div");
+    point.classList.add(`${arguments[i]}Score`);
+    point.textContent = arguments[i];
+    point.style.width = "175px";
+
+    let value = document.createElement("span");
+    value.classList.add(`${arguments[i]}Value`);
+    value.textContent = "0";
+    value.setAttribute("style", "float: right;");
+
+    point.appendChild(value);
+    score.appendChild(point);
+  }
+
+  let round = document.createElement("div");
+  round.classList.add("round");
+  round.textContent = "round";
+  round.setAttribute("style", "width: 175px;margin-top: 10px;");
+
+  let roundValue = document.createElement("span");
+  roundValue.classList.add("roundValue");
+  roundValue.textContent = "0/5";
+  roundValue.setAttribute("style", "float: right;");
+  round.appendChild(roundValue);
+  score.appendChild(round);
 }
 
-function addLabels() {
-  choices.prepend(choiceLabel);
-  score.prepend(scoreLabel);
+function fade() {
+  log.childNodes.forEach(p => {
+    setTimeout(() => {
+      p.style.opacity = parseFloat(p.style.opacity || 1) - 0.2;
+    });
+    if (parseFloat(p.style.opacity) < 0.0) {
+      log.removeChild(p);
+    }
+  });
+}
+
+function playRound(playerSelection, roundNumber, playerScore, comScore) {
+  let computerSelection = computerPlay();
+  updateLog(`the computer chose ${computerSelection}.`, 500);
+  let result = calculator(playerSelection, computerSelection);
+  if (result) {
+    playerScore++;
+    updateLog("you scored.", 1000);
+    updateScore("you");
+  } else if (result === null) {
+    updateLog("neither scored.", 1000);
+  } else {
+    comScore++;
+    updateLog("the computer scored.", 1000);
+    updateScore("computer");
+  }
+  return {
+    roundNumber: roundNumber + 1,
+    playerScore: playerScore,
+    comScore: comScore
+  };
+}
+
+function updateScore(winner) {
+  let value = document.querySelector(`.${winner}Value`);
+  console.log(value);
+  value.textContent = parseInt(value.textContent) + 1;
 }
 
 function computerPlay() {
+  let options = ["rock", "paper", "scissor"];
   let choice = Math.floor(Math.random() * 3) + 0;
   return options[choice];
 }
 
-// Determine whether the PLAYER won or lost
-// true: won
-// false: lost
-// null: tie
 function calculator(pSelection, cSelection) {
   if (pSelection === cSelection) return null;
-  // If same options, return null
   else if (pSelection == "rock") {
-    // If player chose rock,
-    if (cSelection == "paper") return false; // If computer chose paper, player lost
-    return true; // Otherwise player wins
+    if (cSelection == "paper") return false;
+    return true;
   } else if (pSelection == "paper") {
     if (cSelection == "scissor") return false;
     return true;
@@ -253,27 +178,48 @@ function calculator(pSelection, cSelection) {
   }
 }
 
-function darkMode() {
-  let page = document.querySelector("*");
-  page.classList.toggle("darken");
+function createChoices(title) {
+  let choices = document.createElement("div");
+  choices.classList = "choices";
+  let label = document.createElement("label");
+  label.textContent = title;
+  choices.appendChild(label);
 
-  rockBtn.classList.toggle("darkBtn");
-  paperBtn.classList.toggle("darkBtn");
-  scissorBtn.classList.toggle("darkBtn");
-
-  startBtn.forEach(sButton => {
-    sButton.classList.toggle("darkBtn");
-  });
-
-  pScore.classList.toggle("darkScore");
-  cScore.classList.toggle("darkScore");
-  rCount.classList.toggle("darkScore");
+  for (let i = 1; i < arguments.length; i++) {
+    addChoice(choices, arguments[i]);
+  }
+  return choices;
 }
 
-function titleCase(str) {
-  str = str.toLowerCase().split(" ");
-  for (var i = 0; i < str.length; i++) {
-    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-  }
-  return str.join(" ");
+function addChoice(parentNode, name) {
+  let btn = document.createElement("button");
+  btn.textContent = name;
+  btn.classList = `${name}Btn`;
+  if (dark) btn.classList.add("darkBtn");
+  parentNode.appendChild(btn);
+}
+
+function updateLog(prompt, time) {
+  let p = document.createElement("p");
+  p.textContent = prompt;
+  setTimeout(() => {
+    log.prepend(p);
+  }, time || 1000);
+}
+
+function tempDisable(button, time) {
+  button.disabled = true;
+  setTimeout(() => {
+    button.disabled = false;
+  }, time || 1000);
+}
+
+function darkMode() {
+  document.querySelector("body").classList.toggle("darken");
+  dark = !dark;
+  document.querySelector(".title-bar").classList.toggle("dark-bar");
+  let allBtns = document.querySelectorAll("button");
+  allBtns.forEach(btn => {
+    btn.classList.toggle("darkBtn");
+  });
 }
